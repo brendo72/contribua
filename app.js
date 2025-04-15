e63b2786dfa7df4a7b6e4b409155a0f27c005f3e
brendo 12 <brendobraga72@gmail.com>
2025-04-15 16:08:09 -0300

    add: local usuario

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ import express from "express"
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const usuarios = [
+  {id:1,nome:'joao', email:'joao@hotmail.com'},
+  {id:2,nome:'maria', email:'maria@hotmail.com'}
+]
+
+app.get('/usuario', (req, res) => {
+  res.send(usuarios)
 })
 
 app.listen(port, () => {
